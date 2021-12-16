4bd3a7cc0bb58d7841519b86b2963861b5cfc5a8
pallabez <pallabsonowal999@gmail.com>
2021-12-17 01:01:59 +0530

    Setup view enginer, controllers and routers.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use('/', require('./routes/index'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err) {
     if(err) {
