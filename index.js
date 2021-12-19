dc48c2a5dfb424b96fc70271f73facc0f1dc1431
Pallab Sonowal <pallabsonowal999@gmail.com>
2021-12-19 12:18:59 +0530

    Added asset directory and linked statics

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use('/', require('./routes/index'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.urlencoded());    //To parse req.body
+app.use(express.static('assets'));
 
 app.post('/create-task', function(req, res) {
     console.log(req.body);
