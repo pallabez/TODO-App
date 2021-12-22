d5f4875717e789cfb6a98198656bc9e2f5c5a1c5
Pallab Sonowal <pallabsonowal999@gmail.com>
2021-12-22 22:34:06 +0530

    Setup mongoDB

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.urlencoded());
 app.use(express.static('assets'));
-
 app.use('/', require('./routes/index'));
 
 app.listen(port, function(err) {
